ce92f1b9305728794d90777f2b25c041ef0ba3d7
Keegan Farris <keegan.farris@awardco.com>
2025-07-17 15:41:06 -0600

    more bad experiences

--- src/components/useVolumeStore.js
+++ src/components/useVolumeStore.js
@@ -8,7 +8,7 @@ export const useVolumeStore = defineStore('volumeStore',() => {
   const router = useRouter()
   const userIsPremium = ref(false);
   const isViewingAd = ref(false);
-  const volume = ref(100);
+  const volume = ref(0);
 
   watch(isViewingAd, (newValue) => {
     if (isViewingAd.value) {
@@ -24,11 +24,19 @@ export const useVolumeStore = defineStore('volumeStore',() => {
   const isFlipped = ref(false);
   const selectedCardInfo = ref(null);
 
+  function setAdTimeout() {
+    setTimeout(() => {
+      const isVolumeCloserToZero = volume.value < 50;
+      volume.value = isVolumeCloserToZero ? 100 : 0;
+    }, userIsPremium ? 15000 : 10000);
+  }
+
   return {
     userIsPremium,
     isFlipped,
     selectedCardInfo,
     isViewingAd,
     volume,
+    setAdTimeout
   }
 });
